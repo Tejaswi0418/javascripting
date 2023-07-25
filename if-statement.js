67adb02d608a307a2dc47173756aa516c3e13615
Ravuri Teja Nagaswi <111522542+Tejaswi0418@users.noreply.github.com>
2023-07-25 13:51:35 +0530

    Add files via upload

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+var friut="orange";
+
+if(friut.length>5)
+{
+    console.log("The fruit name has more than five characters.");
+}else{
+    console.log("The fruit name has five characters or less.");
+}
+
+
